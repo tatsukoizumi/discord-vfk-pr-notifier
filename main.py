40311b92454773413d9db0c703b17d98395b3df9
tatsukoizumi <koizumitattsuu@gmail.com>
2023-09-02 15:33:54 +0900

    refactoring with enum

--- main.py
+++ main.py
@@ -1,41 +1,77 @@
+from enum import Enum
 import os
+import requests
 
+from bs4 import BeautifulSoup
 from dhooks import Embed, Webhook
 
-import requests
-from bs4 import BeautifulSoup
+class NEWS_TYPE(Enum):
+    MATCH = 1
+    TEAM = 2
+    OTHER = 3
 
-webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
+    @classmethod
+    def get_values(cls) -> list:
+        return [i.value for i in cls]
 
-def post_discord(embed: Embed):
-    hook = Webhook(webhook_url)
-    hook.send(embed=embed)
 
-def get_html() -> [dict]:
-    url = "https://www.ventforet.jp/news/match"
+def webhook_url(news_type: NEWS_TYPE) -> str:
+    if news_type == NEWS_TYPE.MATCH:
+        return os.environ["DISCORD_WEBHOOK_URL_MATCH"]
+    elif news_type == NEWS_TYPE.TEAM:
+        return os.environ["DISCORD_WEBHOOK_URL_TEAM"]
+    elif news_type == NEWS_TYPE.OTHER:
+        return os.environ["DISCORD_WEBHOOK_URL_OTHER"]
+    else:
+        raise Exception("Unknown news type")
+
+def news_path(news_type: NEWS_TYPE) -> str:
+    if news_type == NEWS_TYPE.MATCH:
+        return "/match"
+    elif news_type == NEWS_TYPE.TEAM:
+        return "/team"
+    elif news_type == NEWS_TYPE.OTHER:
+        return "/other"
+    else:
+        raise Exception("Unknown news type")
+
+
+def get_news_items(news_type: NEWS_TYPE) -> [dict]:
+    url = "https://www.ventforet.jp/news/" + news_path(news_type)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
     news_item_elements = soup.find_all("a", class_="newsList__item")
-    items = []
+    news_items = []
     for item in news_item_elements:
         href = item["href"]
         title = item.find(class_="top-news__information__detail").text
         release_id = href.split("/")[-1]
-        path = href.rsplit("/", 1)[0]
         image = item.find(class_="newsList__itemImage").find("img")["src"]
-        items.append({
+        news_items.append({
             "full_url": "/".join([url, release_id]),
             "title": title,
             "id": release_id,
             "image": image,
-            "path": path
         })
-    return items
+    return news_items
+
+
+def get_content(title: str, url: str) -> str:
+    return f"{title}\n{url}"
+
+def get_embed(title: str, url: str, image: str) -> Embed:
+    embed = Embed()
+    embed.set_title(title, url=url)
+    embed.set_image(image)
+    return embed
+
 
 if __name__ == "__main__":
-    items = get_html()
-    for item in items[0:1]:
-        embed = Embed()
-        embed.set_title(item['title'], url=item["full_url"])
-        embed.set_image(item["image"])
-        post_discord(embed)
+    for type in NEWS_TYPE.get_values():
+        webhook_url = webhook_url(type)
+        hook = Webhook(webhook_url)
+        items = get_news_items()
+        for item in items[0:1]:
+            content = get_content(item["title"], item["full_url"])
+            embed = get_embed(item["title"], item["full_url"], item["image"])
+            hook.send(content=content,embed=embed)
