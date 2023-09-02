75180fd9c6da8b3f993deeba47447d8e17a74f6e
tatsukoizumi <koizumitattsuu@gmail.com>
2023-09-02 14:02:09 +0900

    use dhook

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import json
-from urllib.request import Request, urlopen
 import os
+
+from dhooks import Embed, Webhook
 
 webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
 
-def post_discord(message: str, webhook_url: str):
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "DiscordBot (private use) Python-urllib/3.10",
-    }
-    data = {"content": message}
-    request = Request(
-        webhook_url,
-        data=json.dumps(data).encode(),
-        headers=headers,
-    )
-
-    with urlopen(request) as res:
-        assert res.getcode() == 204
+def post_discord(embed: Embed):
+    hook = Webhook(webhook_url)
+    hook.send(embed=embed)
 
 
 if __name__ == "__main__":
-    post_discord("hello", webhook_url)
+    embed = Embed()
+    embed.set_title("hello", url="https://example.com")
+    post_discord(embed)
